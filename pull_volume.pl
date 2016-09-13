1dd12278c7aa454222e05847239f0405a526ea4a
Andrew Morgan <andrew.morgan@icuc.social>
2016-09-13 05:41:43 -0400

    Update pull_volume.pl

--- pull_volume.pl
+++ pull_volume.pl
@@ -4,7 +4,8 @@ use warnings;
 use strict;
 use Mail::Sender;
 
-my ($line,@array);
+my ($line,@array,$limit);
+$limit='50';
 
 @array=`mysql -usproot -psocialpatrol -e 'SELECT SUM(action_count) FROM  processor_action_log WHERE action_code = "SAVE_ENTRIES" AND action_time > DATE_SUB(NOW(), INTERVAL 20 MINUTE);' -hicuc-prod-log.czrsywfk6vbk.us-west-2.rds.amazonaws.com -Dsocial_patrol_logs`;
 
@@ -12,7 +13,7 @@ my ($line,@array);
                 {
                         while ($line =~ /(\d+)/mgx)
                         {
-                                if ( "$1" < '50' )
+                                if ( "$1" < "$limit" )
                                         {
                                         email("Database Log File attached.Amount of content pulled in the last 20mins is less than 50");                                 $
                                         }
